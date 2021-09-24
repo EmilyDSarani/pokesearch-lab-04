39e23d99b2cf9af76cdda2afa36f22d977c05109
EmilyDSarani <emily.sarani@gmail.com>
2021-09-24 13:37:33 -0500

    ALL THE POKEMON MUAHAHAHAHAH

--- src/SearchAndDrop/PokeSearch.js
+++ src/SearchAndDrop/PokeSearch.js
@@ -61,7 +61,7 @@ export default class PokeSearch extends Component {
     //Zack pointed out that instead of grabbing from data to remember that we are grabbing from the API
     fetchSearch = async() =>{
         this.setState({isLoading:true})
-        const pokeball = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.searchQueary}&sort=pokemon&direction=${this.state.sortOrder}`);
+        const pokeball = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.searchQueary}&sort=pokemon&direction=${this.state.sortOrder}&perPage=1000`); //Yosi helped me figure out how to show all the pokemon
         this.setState({pokedex: pokeball.body.results, isLoading:false});  //I made a note that you can put a lot of states in one setState...so I assume one needs to go here
         
     }
